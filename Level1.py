dd34b5102c29f14f954fbd4422e8b8112f70c229
ksssssw <rtd0731@naver.com>
2022-01-12 18:06:42 +0900

    약수의 개수와 덧셈

--- Level1.py
+++ Level1.py
@@ -327,27 +327,53 @@
 ######################################
 #                실패율                #
 ######################################
-def solution(N, stages):
-    answer = []
-    result = {}
-    total_player = len(stages)
+# def solution(N, stages):
+#     answer = []
+#     result = {}
+#     total_player = len(stages)
     
-    for i in range(1, N + 1):
-        if total_player != 0 :
-            cnt = stages.count(i)
-            temp = cnt / total_player
-            result[i] = temp
-            total_player -= cnt
-        else:
-            result[i] = 0
+#     for i in range(1, N + 1):
+#         if total_player != 0 :
+#             cnt = stages.count(i)
+#             temp = cnt / total_player
+#             result[i] = temp
+#             total_player -= cnt
+#         else:
+#             result[i] = 0
+#     # print(result)
+#     # sort_result = dict(sorted(result.items(), key = lambda item : item[1], reverse = True))
+#     # answer = list(sort_result.keys())
     
-    # print(result)
-    # sort_result = dict(sorted(result.items(), key = lambda item : item[1], reverse = True))
-    # answer = list(sort_result.keys())
-    
-    return sorted(result, key = lambda x : result[x], reverse = True)
+#     return sorted(result, key = lambda x : result[x], reverse = True)
 
-N = 4
-# stages = [2, 1, 2, 6, 2, 4, 3, 3]
-stages = [4,4,4,4,4]
-print(solution(N, stages))
+# N = 4
+# # stages = [2, 1, 2, 6, 2, 4, 3, 3]
+# stages = [4,4,4,4,4]
+# print(solution(N, stages))
+
+######################################
+#           약수의 개수와 덧셈           #
+######################################
+def checkNumber(num):
+    cnt = 0
+    for i in range(1, num + 1):
+        if num % i == 0:
+            cnt += 1
+    if cnt % 2 == 0:
+        return True
+    else:
+        return False
+
+def solution(left, right):
+    answer = 0
+    for i in range(left, right + 1):
+        if checkNumber(i) == True:
+            answer += i
+        else:
+            answer -= i
+    return answer
+
+
+left = 13
+right = 17
+print(solution(left, right))
