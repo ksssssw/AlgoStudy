14fcec9d110a74395f756bc8b513883507e75fe5
ksssssw <rtd0731@naver.com>
2022-02-01 20:59:51 +0900

    solving...

--- Baekjoon.py
+++ Baekjoon.py
@@ -2,16 +2,61 @@
 # if name == 'joonas':
 #     print(name+'??!')
 
+################################################
+
 # year = input()
 # print(int(year) - 543)
 
-resultList = []
-while True:
-    a, b = map(int, input().split())
-    if a == 0 and b == 0:
-        break
-    resultList.append(a + b)
+################################################
 
-for i in resultList:
-    print(i)
+# resultList = []
+# while True:
+#     a, b = map(int, input().split())
+#     if a == 0 and b == 0:
+#         break
+#     resultList.append(a + b)
+
+# for i in resultList:
+#     print(i)
+
+################################################
+
+# resultList = []
+# while True:
+#     try:
+#         a, b = list(map(int, input().split()))
+#         resultList.append(a + b)
+#     except:
+#         break
+
+# for i in resultList:
+#     print(i)
+
+################################################
+
+# num = input()
+# new_num = '-1'
+# cnt = 0
+# temp = num
+
+# while True:
+#     if int(num) == int(new_num):
+#         break
+
+#     if int(temp) < 10:
+#         temp = '0' + temp
     
+#     new_num = temp[-1] + str(int(temp[0]) + int(temp[-1]))[-1]
+#     temp = new_num
+#     cnt += 1
+# print(cnt)
+
+################################################
+
+def solution(N, numberList):
+    print('{} {}'.format(min(numberList), max(numberList)))
+
+N = int(input())
+numberList = list(map(int, input().split(' ')))
+print(numberList)
+solution(N, numberList)
--- Level1.py
+++ Level1.py
@@ -562,11 +562,11 @@
 ######################################
 #         가운데 글자 가져오기            #
 ######################################
-def solution(s):
-    if len(s) % 2 == 0:
-        return s[len(s) // 2 - 1:len(s) // 2 + 1]
-    else:
-        return s[len(s) // 2]
+# def solution(s):
+#     if len(s) % 2 == 0:
+#         return s[len(s) // 2 - 1:len(s) // 2 + 1]
+#     else:
+#         return s[len(s) // 2]
     
-s = 'qwer'
-print(solution(s))
+# s = 'qwer'
+# print(solution(s))
