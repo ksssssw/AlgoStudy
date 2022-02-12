de93d53955aa4bc52a6ca9cde98b497528a68f4b
ksssssw <rtd0731@naver.com>
2022-02-12 21:43:56 +0900

    자바로 공부를 시작하려함..

---
+++ AlgoStudyJAVA/src/study/input/example01/Main.java
@@ -0,0 +1,7 @@
+package study.input.example01;
+
+class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
--- Baekjoon.py
+++ Baekjoon.py
@@ -567,10 +567,164 @@
 # dfs를 구현할때 자주 사용됨 매우 중요함!!
 # Recursive Function == 재귀 함수
 # 종료 조건을 잘 넣어줘야함
-def gcd(a, b):
-    if a % b == 0:
-        return b
-    else:
-        return gcd(b, a % b)
+# def gcd(a, b):
+#     if a % b == 0:
+#         return b
+#     else:
+#         return gcd(b, a % b)
         
-print(gcd(192, 162))
+# print(gcd(192, 162))
+# 재귀 함수를 잘 활용하면 복잡한 알고리즘을 간단하게 작성할 수 있지만 다른사람이 이해하기 어려울 수 있음
+
+# DFS(Depth - First - Search) - 깊이 우선 탐색
+# 스택자료구조 또는 재귀함수를 사용
+##### dfs 구현 #####
+# def dfs(graph, v, visited):
+#     visited[v] = True
+#     print(v, end = ' ')
+    
+#     for i in graph[v]:
+#         if not visited[i]:
+#             dfs(graph, i, visited)
+
+# graph = [
+#     [],
+#     [2, 3, 8],
+#     [1, 7],
+#     [1, 4, 5],
+#     [3, 5],
+#     [3, 4],
+#     [7],
+#     [2, 6, 8],
+#     [1, 7]]
+# visited = [False] * 9
+# dfs(graph, 1, visited)
+
+# BFS는 너비우선 탐색
+##### bfs 샘플 #####
+# from collections import deque
+
+# def bfs(graph, start, visited):
+#     queue = deque([start])
+#     visited[start] = True
+    
+#     while queue:
+#         v = queue.popleft()
+#         print(v, end = ' ')
+        
+#         for i in graph[v]:
+#             if not visited[i]:
+#                 queue.append(i)
+#                 visited[i] = True
+
+# graph = [
+#     [],
+#     [2, 3, 8],
+#     [1, 7],
+#     [1, 4, 5],
+#     [3, 5],
+#     [3, 4],
+#     [7],
+#     [2, 6, 8],
+#     [1, 7]]
+# visited = [False] * 9
+# bfs(graph, 1, visited)
+
+##############################################
+# def dfs(x, y):
+#     if x <= -1 or x >= n or y <= -1 or y >= m:
+#         return False
+    
+#     # 현재 노드를 아직 방문하지 않았다면...
+#     if graph[x][y] == 0:
+#         # 해당 노드 방문 처리
+#         graph[x][y] = 1
+#         # 상하좌우의 위치들도 모두 재귀적으로 호출
+#         dfs(x - 1, y)
+#         dfs(x, y - 1)
+#         dfs(x + 1, y)
+#         dfs(x, y + 1)
+#         return True
+#     return False
+
+# n, m = map(int, input().split())
+# graph = []
+# for _ in range(n):
+#     graph.append(list(map(int, input())))
+    
+# result = 0
+# for i in range(n):
+#     for j in range(m):
+#         if dfs(i, j) == True:
+#             result += 1
+            
+# print(result)
+#----------------------------------------------#
+# def dfs(x, y):
+#     if x <= -1 or x >= n or y <= -1 or y >= m:
+#         return False
+
+#     if graph[x][y] == 0:
+#         graph[x][y] = 1
+#         dfs(x-1, y)
+#         dfs(x+1, y)
+#         dfs(x, y-1)
+#         dfs(x, y+1)
+#         return True
+#     return False
+
+# n, m = map(int, input().split())
+# graph = []
+# for _ in range(n):
+#     graph.append(list(map(int, input())))
+    
+# result = 0
+# for i in range(n):
+#     for j in range(m):
+#         if dfs(i, j) == True:
+#             result += 1
+
+# print(result)
+
+## 미로탈출문제... 
+# from collections import deque
+
+# def bfs(x, y):
+#     queue = deque()
+#     queue.append((x, y))
+    
+#     while queue:
+#         x, y = queue.popleft()
+#         # 현재 위치에서 4방향으로 탐색
+#         for i in range(4):
+#             nx = x + dx[i]
+#             ny = y + dy[i]
+            
+#             if nx < 0 or nx >= n or ny < 0 or ny >= m:
+#                 continue
+#             if graph[nx][ny] == 0:
+#                 continue
+#             if graph[nx][ny] == 1:
+#                 graph[nx][ny] = graph[x][y] + 1
+#                 queue.append((nx, ny))
+#     return graph[n - 1][m - 1]
+
+# n, m = map(int, input().split())
+# graph = []
+# for _ in range(n):
+#     graph.append(list(map(int, input())))
+# dx = [-1, 1, 0, 0]
+# dy = [0, 0, -1, 1]
+# print(bfs(0, 0))
+
+################################################
+def solution(N):
+    temp = str(bin(N)[2:])
+    sizeList = temp.split('1')
+    print(temp)
+    if temp.count('1') > 1:
+        return len(max(sizeList))
+    else:
+        return 0
+    
+print(solution(51712))
